65cf9c97082b0b06661247b9d7dfdd214317e4e1
litianyu <m13182371876@163.com>
2022-05-25 03:04:20 +0800

    update login & register

--- src/main/java/com/mooc/controller/BaseServlet.java
+++ src/main/java/com/mooc/controller/BaseServlet.java
@@ -21,11 +21,11 @@ public class BaseServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 1. 获取请求路径
-        String uri = req.getRequestURI();  //   /demo_war/stu/selectAll
-        // 2. 获取最后一段请求路径
+        String uri = req.getRequestURI();  //   /demo_war/stu/login
+        // 2. 获取URI
         int index = uri.lastIndexOf("/");
-        String methodName = uri.substring( index+1 );  // selectAll
-        // 3. 获取StuServlet的字节码对象Class
+        String methodName = uri.substring( index+1 );  // login
+        // 3. 获取字节码对象Class
         // 谁调用this所在的方法，this就代表谁
         Class<? extends BaseServlet> cls = this.getClass();  // StuServlet调用了service方法，那么cls就是StuServlet.class
         // 4. 获取方法method对象
--- src/main/java/com/mooc/controller/StuServlet.java
+++ src/main/java/com/mooc/controller/StuServlet.java
@@ -13,17 +13,23 @@ import java.io.IOException;
 @WebServlet("/stu/*")  // 使用目录式的路径映射
 public class StuServlet extends BaseServlet {
 
-    StuServiceImpl stuService = new StuServiceImpl();   // 提到成员位置
+    StuServiceImpl stuService = new StuServiceImpl();   // service层访问接口
 
     public void register(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");   // 设置编码方式,处理POST请求中文乱码问题
         String jsonStr = request.getReader().readLine();
-        System.out.println(jsonStr);
-        Stu stu = JSON.parseObject(jsonStr, Stu.class);
-        stuService.addStu(stu);  // 注册
-        response.setStatus(200);
-        response.setContentType("text/plaintext;charset=utf-8");
-        response.getWriter().write(String.valueOf(true));
+        Stu stu = JSON.parseObject(jsonStr, Stu.class);   // 映射为Stu实体类对象
+        Stu tmpStu = stuService.selectByUsername(stu.getUsername());
+        if (tmpStu!=null) {   // 如果用户已存在，设置响应状态码500
+            response.setStatus(500);
+            response.setContentType("text/plaintext;charset=utf-8");
+            response.getWriter().write(String.valueOf(false));
+        } else {  // 如果是新用户，则继续进行注册，并返回响应状态码200
+            stuService.addStu(stu);  // 注册
+            response.setStatus(200);
+            response.setContentType("text/plaintext;charset=utf-8");
+            response.getWriter().write(String.valueOf(true));
+        }
     }
 
     public void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -32,12 +38,12 @@ public class StuServlet extends BaseServlet {
         String password = request.getParameter("password");
 
         Stu stu = stuService.selectByUsernameAndPsw(username, password);
-
-        if (stu!=null) {
+        if (stu!=null) {   // 登录信息正确
+            // 设置cookie，在浏览器端保存用户stuId
             Cookie cookie = new Cookie("stuId",  stu.getId() + "");
             response.addCookie(cookie);
             response.setStatus(200);
-        } else {
+        } else {  // 登录信息有误
             response.setStatus(500);
         }
     }
